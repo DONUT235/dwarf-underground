c93d6ab5788075be38d7748e5043ca070e83f46c
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-11 15:06:17 -0400

    Clickbait is now displayed with a map over an array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ClickbaitDiv from './ClickbaitDiv.js'
 import './App.css';
 
 class App extends Component {
@@ -71,34 +72,14 @@ class App extends Component {
 						<p className="ad-text">Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
 					</div>
 					</aside>
-
-					<div className="small-12 columns other-articles">
-					<h2>From around the Realm</h2>
-					<div className="small-6 medium-3 columns other-article">
-						<a href="#">
-						<img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-						<p>Single Orcs in Indianapolis</p>
-						</a>	
-					</div>
-					<div className="small-6 medium-3 columns other-article">
-						<a href="#">
-						<img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
-						<p>You won't believe what's under this mountain</p>
-						</a>	
-					</div>
-					<div className="small-6 medium-3 columns other-article">
-						<a href="#">
-						<img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
-						<p>Mine 20% more gold with One Weird Trick</p>
-						</a> 
-					</div>
-					<div className="small-6 medium-3 columns other-article">
-						<a href="#">
-						<img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
-						<p>Surprise for Indiana Hobbits born before 1999</p>
-						</a> 
-					</div>
-					</div>
+					<ClickbaitDiv 
+						clickbaitArray={[
+							{clickbaitImage:"https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif", clickbaitText:"Single Orcs in Indianapolis"},
+							{clickbaitImage:"https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg", clickbaitText:"You won't believe what's under this mountain"},
+							{clickbaitImage:"http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg", clickbaitText:"Mine 20% more gold with One Weird Trick"},
+							{clickbaitImage:"http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg", clickbaitText:"Surprise for Indiana Hobbits born before 1999"} 
+						]}
+					/>
 				</main>
 
 				<footer className="expanded row">

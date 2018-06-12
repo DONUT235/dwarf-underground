f4f7118b85de48d3dce38c286f700a10e1eff0ae
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-11 21:59:14 -0400

    React-ify the page (wip)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import ClickbaitDiv from './ClickbaitDiv.js'
 import './App.css';
-import CommentLink from './CommentLink.js';
+import Links from './Links.js';
 import CommentSection from './CommentSection.js';
+import Advertisement from './Ad.js';
 
 class App extends Component {
 	constructor(props) {
@@ -54,28 +55,17 @@ class App extends Component {
 
 							<p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
 						</section>
-						<div className="article-links">
+						<Links commentClickHandler={ev => this.revealComments(ev)} />
+						{/*<div className="article-links">
 							<CommentLink onclick={ev => this.revealComments(ev)} />
 							<a className="article-link" href="#">
 							<i className="fa fa-share"></i>
 							<span className="article-link-text">Share Post</span>
 							</a>
-						</div>
+						</div>*/}
 						<CommentSection display={this.state.showComments} />
 					</div>
-					<aside className="large-4 medium-12 columns">
-					<div className="ad">
-						<h3>The Last Hammer You'll Ever Need</h3>
-						<div className="ad-image">
-						<img src="https://s-media-cache-ak0.pinimg.com/originals/b4/d5/ab/b4d5ab4a82b8f7efc1adaf548df1d089.jpg" alt="hammer" />
-						<a href="#">visit sponsor</a>
-						</div>	
-						<h4>Mining diamonds or crushing the skulls of your enemies, the SmashMaster 4000 does it all</h4>
-						<p className="ad-text">Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
-
-						<p className="ad-text">Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
-					</div>
-					</aside>
+					<Advertisement />
 					<ClickbaitDiv 
 						clickbaitArray={[
 							{clickbaitImage:"https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif", clickbaitText:"Single Orcs in Indianapolis"},
@@ -95,7 +85,7 @@ class App extends Component {
 	}
 	revealComments = ev => {
 		ev.preventDefault();
-		this.setState({showComments: true});
+		this.setState({showComments: !this.state.showComments});
 	}
 }
 
---
+++ src/Links.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import CommentLink from './CommentLink.js';
+
+class Links extends Component {
+	render() {
+		return (
+			<div className="article-links">
+				<CommentLink onclick={this.props.commentClickHandler} />
+				<a className="article-link" href="#">
+				<i className="fa fa-share"></i>
+				<span className="article-link-text">Share Post</span>
+				</a>
+			</div>
+		);
+	}
+}
+
+export default Links;
